ee2c5e8517c125fba17fbc1690c6d5c9f5845f68
toopukondabhaskar <bhaskar.t325@gmail.com>
2019-08-02 15:34:57 +0530

    2nd version

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$1" ]
-  then
-    echo "Argument stage is required"
-    exit 1
-fi
+#if [ -z "$1" ]
+#  then
+#    echo "Argument stage is required"
+#    exit 1
+#fi
 
-STAGE=$1
+STAGE=PROD #$1
 SERVICE="some-rando-name"
 BUCKET=devops-poc-artifact # "$s3ArtifactBucketName"
 REGION="us-east-1"
 
-echo "CloudFormation packaging..."
-aws cloudformation package \
+echo "XXXXXXXXXXXXXXXX CloudFormation packaging..."
+sam cloudformation package \
     --region ${REGION} \
     --template-file $samTemplate \
     --output-template-file ${STAGE}-$packagedTemplate \
     --s3-bucket ${BUCKET} \
     --s3-prefix sam/${SERVICE}
 
-echo "CloudFormation deploying..."
-aws cloudformation deploy  \
+echo "YYYYYYYYYYYYYYYY CloudFormation deploying..."
+sam cloudformation deploy  \
     --region ${REGION} \
     --template-file ${STAGE}-$packagedTemplate \
     --stack-name ${STAGE}-${SERVICE} \
